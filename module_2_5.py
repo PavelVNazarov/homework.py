d43c479f703b7716bf84a9d3e235c96fac5e4a55
PavelVNazarov <19pavel76@mail.ru>
2024-07-23 10:53:52 +0500

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,17 @@
+# Домашняя работа по уроку "Функции в Python.Функция с параметром"
+# Назаров ПВ
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        new_w = []
+        for j in range(m):
+            new_w.append(value)
+        matrix.append(new_w)
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
