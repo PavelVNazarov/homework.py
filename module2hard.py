125ffdc2a85e47076d1574fe6b8e4e32fcedead2
PavelVNazarov <19pavel76@mail.ru>
2024-07-25 17:14:29 +0500

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -2,12 +2,12 @@
 # Назаров ПВ
 import random
 
-x = random.randint(3, 20)
+x = random.randint(3, 21)
 key_list = str(x)+' - '
 list_key = str(x)+' - '
-for i in range(1,20):
+for i in range(1,21):
     if i != x and i < x:
-        for j in range(1,20):
+        for j in range(1,21):
             if j != i and j != x and j < x and x % (i + j) == 0:
                 if len(key_list) <= 5:
                     key_list += str(i)+' и '+str(j)
