53e044ab895e6bf670ad0ea3b99a9e74db8d7abf
PavelVNazarov <19pavel76@mail.ru>
2024-07-30 09:51:38 +0500

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -2,7 +2,17 @@
 # Назаров ПВ
 
 def print_params(a = 1, b = 'строка', c = True):
-  print(print_params)
-
+    print(a,b,c)
 
 print_params()
+print_params(12)
+print_params(b = 25) 
+print_params(c = [1,2,3])
+
+values_list = [3,'children',False]
+values_dict = {'c':6.7,'b':'who','a':7}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
