503c7263f7b4ca21fcbb965f2e27f6de2190b8b7
PavelVNazarov <19pavel76@mail.ru>
2024-07-31 23:29:35 +0500

    Add files via upload

---
+++ module_3_hard.py
@@ -0,0 +1,28 @@
+# Дополнительное практическое задание по модулю: "Подробнее о функциях."
+# Назаров ПВ
+def calculate_structure_sum(*args):
+  * pass
+
+def calculate_structure_sum_2(**kwargs):
+  * pass
+
+def calculate_structure_sum_3(*args, **kwargs):
+  *pass
+
+
+
+
+
+
+
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
