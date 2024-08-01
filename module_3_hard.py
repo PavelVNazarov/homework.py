788610f5fdc46269c5471d55ad1de104235ca57c
PavelVNazarov <19pavel76@mail.ru>
2024-08-01 12:01:29 +0500

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,19 +1,28 @@
 # Дополнительное практическое задание по модулю: "Подробнее о функциях."
 # Назаров ПВ
-def calculate_structure_sum(*args):
-  * pass
 
-def calculate_structure_sum_2(**kwargs):
-  * pass
+import inspect
 
-def calculate_structure_sum_3(*args, **kwargs):
-  *pass
+def calculate_structure_sum(args):
+ # print(args)
+  count = 0 # переменная счетчика
+  # params = locals().keys()
+  # # Сразу копируйте ключи, пока locals() не наполнился другими переменными!
+  # param_list = list(params)
+  # # Продолжение функции
+  # print(param_list)
+  for i in range(len(args)):
+    # определили тип аргументов
+    print("\nData type of argument: ",type(args[i]),args[i])
+    if type(args[i]) == int:
+      count += args[i]
+    elif type(args[i]) == str:
+      count += len(args[i])
+    elif type(args[i]) == list:
+      count += calculate_structure_sum(args[i])
+    #elif type(args[i]) == dict:
 
-
-
-
-
-
+  return count
 
 
 data_structure = [
@@ -23,6 +32,7 @@ data_structure = [
 "Hello",
 ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
+
 
 result = calculate_structure_sum(data_structure)
 print(result)
