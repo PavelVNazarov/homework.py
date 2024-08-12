8fb6508cae8f7d7045542b1b69b3bcebe72b093b
PavelVNazarov <19pavel76@mail.ru>
2024-08-12 15:59:52 +0500

    Update module_5_4.py

--- module_5/module_5_4.py
+++ module_5/module_5_4.py
@@ -1,62 +1,59 @@
 # Домашняя работа по уроку "Различие атрибутов класса и экземпляра."
 # Назаров ПВ
+class House:
+    houses_history = []
 
-houses_history = []
-
-    def __new__(cls,*args,**kwargs):
+    def __new__(cls, *args, **kwargs):
         cls.houses_history.append(args[0])
         return object.__new__(cls)
 
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-       # House.houses_history.append(self.name)
 
     def __del__(self):
-        #House.houses_history.remove(self.name)
-        print(self.name," снесён, но он останется в истории")
+        print(self.name, " снесён, но он останется в истории")
 
-  
     def __len__(self):
         return self.number_of_floors
 
     def __str__(self):
         h = 'Название {self.name} количество этажей {number_of_floors}'
-        return h.format(self=self,   number_of_floors=self.number_of_floors)
+        return h.format(self=self, number_of_floors=self.number_of_floors)
 
     def __eq__(self, other):
-      if isinstance(other, House):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+           return self.number_of_floors == other.number_of_floors
 
     def __add__(self, value):
-      if isinstance(self, House) and isinstance(value, int):
-        self.number_of_floors += value
-        return self
+        if isinstance(self, House) and isinstance(value, int):
+            self.number_of_floors += value
+            return self
 
     def __radd__(self, value):
-      return House(self.name, self.number_of_floors + value)
+        return House(self.name, self.number_of_floors + value)
 
     def __iadd__(self, value):
-      return House(self.name, self.number_of_floors + value)
+        return House(self.name, self.number_of_floors + value)
 
     def __lt__(self, other):
-      if isinstance(self, House) and isinstance(other, House):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
 
     def __gt__(self, other):
-      if isinstance(self, House) and isinstance(other, House):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
 
     def __le__(self, other):
-      if isinstance(self, House) and isinstance(other, House):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(self, House) and isinstance(other, House):
+           return self.number_of_floors <= other.number_of_floors
 
     def __ge__(self, other):
-      return self.number_of_floors >= other.number_of_floors
+        return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-      if isinstance(self, House) and isinstance(other, House):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(self, House) and isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
 
 
 h1 = House('ЖК Эльбрус', 10)
