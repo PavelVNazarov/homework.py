a6a3ee50b76b1aaf26d9918516f5d5e59cd4b6b4
PavelVNazarov <19pavel76@mail.ru>
2024-08-12 16:00:55 +0500

    Update module_5_3.py

--- module_5/module_5_3.py
+++ module_5/module_5_3.py
@@ -1,18 +1,10 @@
-# Домашняя работа по уроку "Различие атрибутов класса и экземпляра."
+# Домашняя работа по уроку "Перегрузка операторов."
 # Назаров ПВ
+
 class House:
-    houses_history = []
-
-    def __new__(cls, *args, **kwargs):
-        cls.houses_history.append(args[0])
-        return object.__new__(cls)
-
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-
-    def __del__(self):
-        print(self.name, " снесён, но он останется в истории")
 
     def __len__(self):
         return self.number_of_floors
@@ -22,52 +14,58 @@ class House:
         return h.format(self=self, number_of_floors=self.number_of_floors)
 
     def __eq__(self, other):
-        if isinstance(other, House):
-           return self.number_of_floors == other.number_of_floors
-
+      if isinstance(other, House):
+        return self.number_of_floors == other.number_of_floors
+      
     def __add__(self, value):
-        if isinstance(self, House) and isinstance(value, int):
-            self.number_of_floors += value
-            return self
+      if isinstance(self, House) and isinstance(value, int):
+        self.number_of_floors += value
+        return self
 
     def __radd__(self, value):
-        return House(self.name, self.number_of_floors + value)
-
+      return House(self.name, self.number_of_floors + value)
+      
     def __iadd__(self, value):
-        return House(self.name, self.number_of_floors + value)
+      return House(self.name, self.number_of_floors + value)
 
     def __lt__(self, other):
-        if isinstance(self, House) and isinstance(other, House):
-            return self.number_of_floors < other.number_of_floors
+      if isinstance(self, House) and isinstance(other, House):
+        return self.number_of_floors < other.number_of_floors
 
     def __gt__(self, other):
-        if isinstance(self, House) and isinstance(other, House):
-            return self.number_of_floors > other.number_of_floors
+      if isinstance(self, House) and isinstance(other, House):
+        return self.number_of_floors > other.number_of_floors
 
     def __le__(self, other):
-        if isinstance(self, House) and isinstance(other, House):
-           return self.number_of_floors <= other.number_of_floors
+      if isinstance(self, House) and isinstance(other, House):
+        return self.number_of_floors <= other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+      return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        if isinstance(self, House) and isinstance(other, House):
-            return self.number_of_floors != other.number_of_floors
-
+      if isinstance(self, House) and isinstance(other, House):
+        return self.number_of_floors != other.number_of_floors
+  
 
 h1 = House('ЖК Эльбрус', 10)
-print(House.houses_history)
 h2 = House('ЖК Акация', 20)
-print(House.houses_history)
-h3 = House('ЖК Матрёшки', 20)
-print(House.houses_history)
 
-# Удаление объектов
-del h2
-del h3
+print(h1)
+print(h2)
 
-print(House.houses_history)
+print(h1 == h2)
+
+h1 = h1 + 10 
+print(h1)
+print(h1 == h2)
+
+h1 += 10 
+print(h1)
+
+h2 = 10 + h2 
+print(h2)
+
 print(h1 > h2) 
 print(h1 >= h2) 
 print(h1 < h2) 
