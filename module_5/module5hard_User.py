aecf6c1f7eb8ee86e8f05fe5aaf45a750ac2fb86
PavelVNazarov <19pavel76@mail.ru>
2024-08-15 13:18:42 +0500

    Update module5hard_User.py

--- module_5/module5hard_User.py
+++ module_5/module5hard_User.py
@@ -1,40 +1,46 @@
 # Дополнительное практическое задание по модулю: "Классы и объекты."
 # Назаров ПВ
-# module5hard.py
-
+# module5hard_User.py
 
 class User:
     # Класс пользователь, содержащий атрибуты: логин и пароль
 
-    def __init__(self, nickname, pasword, age):
-        self.username = nickname
+    current_user = None
+    users = []
+    User = []
+
+    def __init__(self,
+                 nickname,
+                 # pasword = hash('pasword'),
+                 pasword,
+                 age):
+        self.nickname = nickname
         self.pasword = pasword
         self.age = age
+        # self.User = [nickname, pasword, age]
+        # self.users.append(self.User)
+        # User.current_user = nickname
+        # print(User.current_user)
 
+    # def __str__(self):
+    #     h = '{nickname}, {pasword}, {age}.'
+    #     return h.format(self = self,
+    #                     nickname = self.nickname,
+    #                     pasword = self.pasword,
+    #                     age = self.age)
 
+    def register(nickname, pasword, age):
+        Us = User(nickname, pasword, age)
+        if len(User.users) == 0:
+            User.users.append(Us)
+            User.current_user = nickname
+        else:
+            for i in range(len(User.users)):
+                if nickname == User.users[i].nickname:
+                    if pasword != User.users[i].pasword:
+                        print(f'Пользователь {nickname} уже существует')
+                else:
+                    User.current_user = User.users[i].nickname
+                    User.users.append(Us)
+        return User.current_user
 
-
-ur = UrTube()
-v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
-
-# Добавление видео
-ur.add(v1, v2)
-
-# Проверка поиска
-print(ur.get_videos('лучший'))
-print(ur.get_videos('ПРОГ'))
-
-# Проверка на вход пользователя и возрастное ограничение
-ur.watch_video('Для чего девушкам парень программист?')
-ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-ur.watch_video('Для чего девушкам парень программист?')
-ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-ur.watch_video('Для чего девушкам парень программист?')
-
-# Проверка входа в другой аккаунт
-ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-print(ur.current_user)
-
-# Попытка воспроизведения несуществующего видео
-ur.watch_video('Лучший язык программирования 2024 года!')
