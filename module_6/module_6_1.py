1a31e4532624961b87a55ec9e60e496bb7a9c33d
PavelVNazarov <19pavel76@mail.ru>
2024-08-28 17:23:40 +0500

    Update module_6_1.py

--- module_6/module_6_1.py
+++ module_6/module_6_1.py
@@ -9,7 +9,7 @@ class Animal():
 
     def eat(self, food):
         if food.edible:
-            print(f'{self.name} ест {food.name}')
+            print(f'{self.name} съел {food.name}')
             self.fed = True
         else:
             print(f'{self.name} не стал есть {food.name}')
