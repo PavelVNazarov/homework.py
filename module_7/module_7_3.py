f7e82fbbba369a291ed45318b8905a9161f238e3
PavelVNazarov <19pavel76@mail.ru>
2024-09-09 15:00:59 +0500

    Update module_7_3.py

--- module_7/module_7_3.py
+++ module_7/module_7_3.py
@@ -28,20 +28,28 @@ class WordsFinder():
         return all_words
 
     def find(self, word):
-        all_words = []
-        str_word = ''
-        for i in self.file_names:
-            with open(i, encoding = 'utf-8') as file:
-                for j in file:
-                    j = j.lower()
-                    str_word +=' '.join(j.split())
-                    index = str_word.index(word.lower())
-                    #if word.lower() in j.lower():
-                    all_words = {i: index}
-                    break
-                    break
-        return all_words
+        index_words = []
+        str_word = self.get_all_words()
+        for i,j in str_word.items():
+            #if word in j:
+            index = j.index(word.lower())+1
+            break
+        index_words = {i: index}
+        return index_words
 
+    # def count(self, word):
+    #     count_words = {}
+    #     count_word = 0
+    #     #str_word = finder2.get_all_words()
+    #     for i in self.file_names:
+    #         with open(i, encoding = 'utf-8') as file:
+    #             for j in file:
+    #                 str_word +=''.join(j.split())
+    #                 str_word = str_word.lower()
+    #                 count_word = j.count(word.lower())
+    #     count_words = {i: count_word}
+    #     return count_words
+        
     def count(self, word):
         all_words = {}
         count_word = 0
@@ -55,6 +63,34 @@ class WordsFinder():
         all_words = {i: count_word}
         return all_words
 
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+
+finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt')
+print(finder1.get_all_words())
+print(finder1.find('captain'))
+print(finder1.count('captain'))
+
+finder1 = WordsFinder('Rudyard Kipling - If.txt',)
+
+print(finder1.get_all_words())
+print(finder1.find('if'))
+print(finder1.count('if'))
+
+finder1 = WordsFinder('Mother Goose - Monday’s Child.txt',)
+print(finder1.get_all_words())
+print(finder1.find('Child'))
+print(finder1.count('Child'))
+
+finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
+      'Rudyard Kipling - If.txt',
+      'Mother Goose - Monday’s Child.txt')
+print(finder1.get_all_words())
+print(finder1.find('the'))
+print(finder1.count('the'))
 
 
 finder2 = WordsFinder('test_file.txt')
