e0c41da22e352611c4333fe43db5665c3b4b4746
PavelVNazarov <19pavel76@mail.ru>
2024-09-13 14:12:02 +0500

    Create module_8_1.py

---
+++ module_8/module_8_1.py
@@ -0,0 +1,14 @@
+# Домашнее задание по теме "Try и Except".
+# Назаров ПВ
+# module_8_1.py
+
+def add_everything_up(a, b):
+    try:
+        return round(a + b,3)
+    except TypeError:
+        return str(a)+str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
