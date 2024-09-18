b103432d1338d86a81f73f04f1a093b3f1a0f5d6
PavelVNazarov <19pavel76@mail.ru>
2024-09-18 21:46:43 +0500

    Update module_9_6.py

--- module_9/module_9_6.py
+++ module_9/module_9_6.py
@@ -3,7 +3,13 @@
 # module_9_6.py
 
 def all_variants(text):
-    yield(text)
+     for i in range(1,len(text)+1):
+          char = 0
+          while char < len(text):
+              if char + i <= len(text):
+                  yield(text[char:char+i])
+              char += 1
+
 
 a = all_variants("abc")
 for i in a:
