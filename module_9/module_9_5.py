11fd36eac3c1f08ddf1955d8fa1db3736c0eebad
PavelVNazarov <19pavel76@mail.ru>
2024-09-18 15:27:14 +0500

    Update module_9_5.py

--- module_9/module_9_5.py
+++ module_9/module_9_5.py
@@ -6,26 +6,36 @@ class StepValueError(ValueError):
     pass
 
 class Iterator():
+
     def __init__(self, start, stop, step = 1):
-        self.start = start
-        self.stop = stop
         if step != 0:
             self.step = step
         else:
-            StepValueError('шаг не может быть равен 0')
+            raise StepValueError('шаг не может быть равен 0')
+        self.start = start
+        self.stop = stop
 
     def __iter__ (self):
         self.pointer = self.start
         return self
 
     def __next__(self):
-
-        if self.pointer < self.start or self.pointer > self.stop:
-            raise StopIteration()
-        self.pointer += self.step
+        self.pointer = self.start
+        self.start += self.step
+        if self.step < 0:
+            if self.pointer < self.stop:
+                raise StopIteration()
+        else:
+            if self.pointer > self.stop:
+                raise StopIteration()
         return self.pointer
 
-""""""
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
 
 iter2 = Iterator(-5, 1)
 iter3 = Iterator(6, 15, 2)
