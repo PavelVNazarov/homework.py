fbe22d56f9c5ec7a581e95fa7a41d47bd45d44d2
PavelVNazarov <19pavel76@mail.ru>
2024-09-18 22:49:31 +0500

    Update module_9_7.py

--- module_9/module_9_7.py
+++ module_9/module_9_7.py
@@ -2,9 +2,25 @@
 # Назаров ПВ
 # module_9_7.py
 
-def sum_three(*numbers):
-     return (sum(numbers))
+def is_prime(func):
+     def my_func(*numbers):
+         result = func(*numbers)
+         k = 0
+         for i in range(2, result// 2 + 1):
+             if (result % i == 0):
+                 k += 1
+         if k <= 0:
+             print("Простое")
+         else:
+             print("Составное")
+         return result
+     return my_func
 
+#@is_prime
+def sum_three(*numbers):
+    return (sum(numbers))
+
+sum_three = is_prime(sum_three)
 
 result = sum_three(2, 3, 6)
 print(result)
