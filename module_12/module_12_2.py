4ff0dfa655d8a200c9fc7eb6f304609d328b1e0d
PavelVNazarov <19pavel76@mail.ru>
2024-10-02 12:43:05 +0500

    Update module_12_2.py

--- module_12/module_12_2.py
+++ module_12/module_12_2.py
@@ -5,46 +5,44 @@
 import unittest
 from runner import Runner, Tournament
 
-class TournamentTest(unittest.TestCase):
-    all_results = {}
-
+class  TournamentTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
 
+    @classmethod
+    def tearDownClass(cls):
+        for key in cls.all_results:
+            new_result = {}
+            for name, result in cls.all_results[key].items():
+                new_result[name] = result.name
+            print(new_result)
+
     def setUp(self):
-        # Создаём объекты бегунов
         self.runner1 = Runner("Усэйн", 10)
         self.runner2 = Runner("Андрей", 9)
         self.runner3 = Runner("Ник", 3)
 
-    @classmethod
-    def tearDownClass(cls):
-        new_result = {}
-        for name, result in cls.all_results.items():
-            new_result[name] = result.name
-            print(new_result)
-
     def test_race_usain_vs_nik(self):
         tournament = Tournament(90, self.runner1, self.runner3)
         results = tournament.start()
-        self.all_results.update(results)
-        last_runner = self.all_results[max(self.all_results.keys())]
-        self.assertTrue(last_runner.name != "Усэйн", f"Последний бегущий: {last_runner.name} не равен ожидаемому 'Усэйн'")
+        self.all_results[1] = results
+        last_runner = results[max(list(results))]
+        self.assertTrue(last_runner, "Усэйн")# f"Последний бегущий: {last_runner} не равен ожидаемому 'Усэйн'")
 
     def test_race_andrey_vs_nik(self):
         tournament = Tournament(90, self.runner2, self.runner3)
         results = tournament.start()
-        self.all_results.update(results)
-        last_runner = self.all_results[max(self.all_results.keys())]
-        self.assertTrue(last_runner.name != "Андрей", f"Последний бегущий: {last_runner.name} не равен ожидаемому 'Андрей'")
+        self.all_results[2] = results
+        last_runner = results[max(list(results))]
+        self.assertTrue(last_runner, "Андрей")# f"Последний бегущий: {last_runner} не равен ожидаемому 'Андрей'")
 
     def test_race_usain_vs_andrey_vs_nik(self):
         tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
         results = tournament.start()
-        self.all_results.update(results)
-        last_runner = self.all_results[max(self.all_results.keys())]
-        self.assertTrue(last_runner.name != "Усэйн", f"Последний бегущий: {last_runner.name} не равен ожидаемому 'Усэйн'")
+        self.all_results[3] = results
+        last_runner = results[max(list(results))]
+        self.assertTrue(last_runner, "Усэйн")  #, f"Последний бегущий: {last_runner} не равен ожидаемому 'Усэйн'")
 
 if __name__ == '__main__':
     unittest.main()
