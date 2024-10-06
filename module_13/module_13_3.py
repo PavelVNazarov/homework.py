408130c288c262a18656066ed44bbac5414aefc7
PavelVNazarov <19pavel76@mail.ru>
2024-10-06 18:16:19 +0500

    Create module_13_3.py

---
+++ module_13/module_13_3.py
@@ -0,0 +1,27 @@
+# Домашнее задание по теме "Методы отправки сообщений".
+# Назаров ПВ
+# module_13_3.py
+
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram import MemoryStorage
+from aiogram import executor
+
+# UrUnNazBot чатбот в Телеграм
+API_TOKEN = 'YOUR_API_TOKEN'  # Замени 'YOUR_API_TOKEN' на токен твоего бота
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    response_text = 'Привет! Я бот, помогающий твоему здоровью.'
+    await message.reply(response_text)
+
+@dp.message_handler(lambda message: True)
+async def all_messages(message: types.Message):
+    response_text = 'Введите команду /start, чтобы начать общение.'
+    await message.reply(response_text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
