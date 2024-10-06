e8f956814fb73d2c9e9c6e06adf8ca9536e9711a
PavelVNazarov <19pavel76@mail.ru>
2024-10-06 17:22:37 +0500

    Update tests_12_3.py

--- module_12/tests_12_3.py
+++ module_12/tests_12_3.py
@@ -4,8 +4,37 @@
 
 from runner import Runner, Tournament
 import unittest
+from functools import wraps
+
+def skip_if_frozen(test_case):
+    @wraps(test_case)
+    def wrapper(self, *args, **kwargs):
+        if getattr(self, 'is_frozen', False):
+            self.skipTest('Тесты в этом кейсе заморожены')
+        return test_case(self, *args, **kwargs)
+    return wrapper
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False  # Меняйте на True, чтобы заморозить тесты
+
+    @skip_if_frozen
+    def test_challenge(self):
+        # Ваш код теста здесь
+        pass  # пример заполнения
+
+    @skip_if_frozen
+    def test_run(self):
+        # Ваш код теста здесь
+        pass  # пример заполнения
+
+    @skip_if_frozen
+    def test_walk(self):
+        # Ваш код теста здесь
+        pass  # пример заполнения
+
 
 class TournamentTest(unittest.TestCase):
+    is_frozen = True  # Меняйте на False, чтобы выполнить тесты
 
     all_results = {}
 
@@ -23,34 +52,31 @@ class TournamentTest(unittest.TestCase):
         for result in cls.all_results.values():
             print(result)
 
+    @skip_if_frozen
     def test_race_usain_and_nik(self):
         tournament = Tournament(90, self.runner1, self.runner3)
         results = tournament.start()
         for num, result in results.items():
             results[num] = result.name
         self.all_results[len(self.all_results) + 1] = results
-
-        # Проверяем, что Ник всегда последний
         self.assertTrue(results[max(results.keys())] == "Ник")
 
+    @skip_if_frozen
     def test_race_andrey_and_nik(self):
         tournament = Tournament(90, self.runner2, self.runner3)
         results = tournament.start()
         for num, result in results.items():
             results[num] = result.name
         self.all_results[len(self.all_results) + 1] = results
-
-        # Проверяем, что Ник всегда последний
         self.assertTrue(results[max(results.keys())] == "Ник")
 
+    @skip_if_frozen
     def test_race_usain_andrey_and_nik(self):
         tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
         results = tournament.start()
         for num, result in results.items():
             results[num] = result.name
         self.all_results[len(self.all_results) + 1] = results
-
-        # Проверяем, что Ник всегда последний
         self.assertTrue(results[max(results.keys())] == "Ник")
 
 
