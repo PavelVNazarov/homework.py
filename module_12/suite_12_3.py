e27d53dc93412cf7277db83e80599e4eb3a46df5
PavelVNazarov <19pavel76@mail.ru>
2024-10-06 17:21:25 +0500

    Update suite_12_3.py

--- module_12/suite_12_3.py
+++ module_12/suite_12_3.py
@@ -1,59 +1,15 @@
 # Домашнее задание по теме "Систематизация и пропуск тестов".
 # Назаров ПВ
 # suite_12_3.py
+
 import unittest
+from tests_12_3 import TournamentTest, RunnerTest
 
-import tests_12_3
-# Декоратор для пропуска тестов
-def skip_if_frozen(func):
-    def wrapper(self, *args, **kwargs):
-        if getattr(self, 'is_frozen', False):
-            self.skipTest("Тесты в этом кейсе заморожены")
-        return func(self, *args, **kwargs)
-    return wrapper
+# Создаем объект TestSuite и добавляем туда тесты
+test_suite = unittest.TestSuite()
+test_suite.addTests(unittest.loader.TestLoader().loadTestsFromTestCase(RunnerTest))
+test_suite.addTests(unittest.loader.TestLoader().loadTestsFromTestCase(TournamentTest))
 
-# Модификация тестов
-class RunnerTest(unittest.TestCase):
-    is_frozen = False  # Измените это значение на True для пропуска всех тестов
-
-    @skip_if_frozen
-    def test_challenge(self):
-        # Ваш тест
-        pass
-
-    @skip_if_frozen
-    def test_run(self):
-        # Ваш тест
-        pass
-
-    @skip_if_frozen
-    def test_walk(self):
-        # Ваш тест
-        pass
-
-class TournamentTest(unittest.TestCase):
-    is_frozen = True  # Измените это значение на False для выполнения всех тестов
-
-    @skip_if_frozen
-    def test_first_tournament(self):
-        # Ваш тест
-        pass
-
-    @skip_if_frozen
-    def test_second_tournament(self):
-        # Ваш тест
-        pass
-
-    @skip_if_frozen
-    def test_third_tournament(self):
-        # Ваш тест
-        pass
-
-if __name__ == '__main__':
-    suite = unittest.TestSuite()
-    # Добавляем тесты в тестовый набор
-    suite.addTest(unittest.makeSuite(RunnerTest))
-    suite.addTest(unittest.makeSuite(TournamentTest))
-
-    runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(suite)
+# Создание TextTestRunner с verbosity=2
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(test_suite)
