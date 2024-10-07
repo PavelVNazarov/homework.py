228e37e6b5c57b6fb1f495204df986825850be37
PavelVNazarov <19pavel76@mail.ru>
2024-10-07 20:42:35 +0500

    Create module_13_6.py

---
+++ module_13/module_13_6.py
@@ -0,0 +1,94 @@
+Домашнее задание по теме "Инлайн клавиатуры".
+Назаров ПВ
+# module_13_6.py
+
+
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram import MemoryStorage
+from aiogram import executor
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StateGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+# UrUnNazBot чатбот в Телеграм
+API_TOKEN = 'YOUR_API_TOKEN'  # Замените 'YOUR_API_TOKEN' на токен Вашего бота
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StateGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+# Создание обычной клавиатуры
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+button_calculate = KeyboardButton('Рассчитать')
+button_info = KeyboardButton('Информация')
+keyboard.add(button_calculate, button_info)
+
+# Создание Inline-клавиатуры
+inline_keyboard = InlineKeyboardMarkup()
+button_calories = InlineKeyboardButton('Рассчитать норму калорий', callback_data='calories')
+button_formulas = InlineKeyboardButton('Формулы расчёта', callback_data='formulas')
+inline_keyboard.add(button_calories, button_formulas)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    response_text = 'Привет! Я бот, помогающий твоему здоровью.'
+    await message.reply(response_text, reply_markup=keyboard)
+
+@dp.message_handler(lambda message: message.text == 'Рассчитать')
+async def main_menu(message: types.Message):
+    await message.reply('Выберите опцию:', reply_markup=inline_keyboard)
+
+@dp.callback_query_handler(lambda call: call.data == 'calories')
+async def set_age(call: types.CallbackQuery):
+    await call.message.reply('Введите свой возраст:')
+    await UserState.age.set()  # Устанавливаем состояние
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)  # Сохраняем возраст
+    await message.reply('Введите свой рост (в см):')
+    await UserState.growth.set()  # Переход к следующему состоянию
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)  # Сохраняем рост
+    await message.reply('Введите свой вес (в кг):')
+    await UserState.weight.set()  # Переход к следующему состоянию
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)  # Сохраняем вес
+
+    data = await state.get_data()  # Получаем все сохраненные данные
+    age = int(data.get('age'))
+    growth = int(data.get('growth'))
+    weight = int(data.get('weight'))
+
+    # Формула Миффлина - Сан Жеора для женщин
+    calories = 10 * weight + 6.25 * growth - 5 * age + 161  # Адаптировать для мужчин при необходимости
+
+    await message.reply(f'Ваша норма калорий: {calories:.2f} ккал')
+    await state.finish()  # Завершаем состояние
+
+@dp.callback_query_handler(lambda call: call.data == 'formulas')
+async def get_formulas(call: types.CallbackQuery):
+    message = "Формула Миффлина-Сан Жеора:\n\n" \
+              "Для женщин: 10 * вес (кг) + 6.25 * рост (см) - 5 * возраст (лет) + 161\n" \
+              "Для мужчин: 10 * вес (кг) + 6.25 * рост (см) - 5 * возраст (лет) + 5"
+    await call.message.reply(message)
+
+@dp.message_handler(lambda message: message.text == 'Информация')
+async def info(message: types.Message):
+    await message.reply('Информация о боте: Я предназначен для расчета калорий и улучшения Вашего здоровья.')
+
+@dp.message_handler(lambda message: True)
+async def all_messages(message: types.Message):
+    await message.reply('Введите команду /start или нажмите на кнопку, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
