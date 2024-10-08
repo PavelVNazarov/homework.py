8eb0ab28d09fbcc2ed9ec03348d105b44c5b5d6d
PavelVNazarov <19pavel76@mail.ru>
2024-10-08 21:21:48 +0500

    Update module_14_1.py

--- module_14/module_14_1.py
+++ module_14/module_14_1.py
@@ -34,7 +34,7 @@ users = [("User1", "example1@gmail.com", 10, 1000),
 cursor.executemany("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", users)
 
 # Обновляем balance у каждой 2-й записи начиная с 1-й на 500
-cursor.execute("UPDATE Users SET balance = 500 WHERE id % 21")
+cursor.execute("UPDATE Users SET balance = 500 WHERE id % 2")
 
 # Удаляем каждую 3-ю запись начиная с 1-й
 cursor.execute("DELETE FROM Users WHERE id % 3 == 1")
