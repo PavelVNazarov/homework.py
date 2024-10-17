779966d691c023dc467c98c9aab9f9b8a2e87c90
PavelVNazarov <19pavel76@mail.ru>
2024-10-17 16:53:48 +0500

    Update module_14_5.py

--- module_14/module_14_5.py
+++ module_14/module_14_5.py
@@ -3,66 +3,73 @@
 # module_14_5.py
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import executor
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher import State, StatesGroup
-from aiogram.utils import executor
-from crud_functions import initiate_db, add_user, is_included, get_all_products
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import aiohttp
+from crud_functions import initiate_db, get_all_products, add_user, is_included
 
-API_TOKEN = 'YOUR_API_TOKEN'
-
+API_TOKEN = 'YOUR_API_TOKEN'  # Замените на Ваш токен
 bot = Bot(token=API_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+    sex = State()
 
 class RegistrationState(StatesGroup):
     username = State()
     email = State()
     age = State()
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.reply("Добро пожаловать!")
+# Инициализация базы данных
+initiate_db()
 
-@dp.message_handler(lambda message: message.text == "Регистрация")
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+button_calculate = KeyboardButton('Рассчитать')
+button_info = KeyboardButton('Информация')
+button_buy = KeyboardButton('Купить')
+button_register = KeyboardButton('Регистрация')  # Кнопка регистрации
+keyboard.add(button_calculate, button_info, button_buy, button_register)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    response_text = 'Привет! Я бот, помогающий твоему здоровью.'
+    await message.reply(response_text, reply_markup=keyboard)
+
+@dp.message_handler(lambda message: message.text == 'Регистрация')
 async def sing_up(message: types.Message):
+    await RegistrationState.username.set()  # Устанавливаем состояние для username
     await message.reply("Введите имя пользователя (только латинский алфавит):")
-    await RegistrationState.username.set()
 
 @dp.message_handler(state=RegistrationState.username)
 async def set_username(message: types.Message, state: FSMContext):
     username = message.text
-    if not is_included(username):
-        await state.update_data(username=username)
-        await message.reply("Введите свой email:")
-        await RegistrationState.email.set()
+    if is_included(username):
+        await message.reply("Пользователь существует, введите другое имя.")
     else:
-        await message.reply("Пользователь существует, введите другое имя:")
-        await RegistrationState.username.set()
+        await state.update_data(username=username)
+        await RegistrationState.next()  # Переход к следующему состоянию
+        await message.reply("Введите свой email:")
 
 @dp.message_handler(state=RegistrationState.email)
 async def set_email(message: types.Message, state: FSMContext):
     email = message.text
     await state.update_data(email=email)
+    await RegistrationState.next()  # Переход к следующему состоянию
     await message.reply("Введите свой возраст:")
-    await RegistrationState.age.set()
 
 @dp.message_handler(state=RegistrationState.age)
 async def set_age(message: types.Message, state: FSMContext):
     age = message.text
-    data = await state.get_data()
-    username = data.get("username")
-    email = data.get("email")
+    user_data = await state.get_data()
+    username = user_data.get("username")
+    email = user_data.get("email")
 
-    add_user(username, email, age)
+    add_user(username, email, age)  # Добавляем пользователя в БД
     await message.reply("Вы успешно зарегистрированы!")
-    await state.finish()
-
-if __name__ == '__main__':
-    initiate_db()
-    populate_initial_products()  # Чтобы добавить начальные продукты
-    executor.start_polling(dp, skip_updates=True)
-Объяснение
-Функции базы данных: Мы создали функции для инициализации базы данных и выполнения CRUD операций.
-Страны регистрации: Определены состояния для ввода данных пользователя.
-Обработчики сообщений: Реализованы функции, чтобы передавать данные через состояния.
+    await state.finish()  # Завершаем состояние
