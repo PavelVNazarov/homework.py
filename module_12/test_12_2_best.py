e99407c3d1dedc47fc22b616d733e3a5c35417ab
PavelVNazarov <19pavel76@mail.ru>
2024-10-22 11:56:45 +0500

    Update and rename module_12_2_best.py to test_12_2_best.py

---
+++ module_12/test_12_2_best.py
@@ -0,0 +1,61 @@
+# Домашнее задание по теме "Методы Юнит-тестирования"
+# Назаров ПВ
+# test_12_2_best.py
+
+from runner_and_tournament import Runner, Tournament
+import unittest
+import itertools
+
+class TournamentTest(unittest.TestCase):
+
+    all_results = {}
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runners = [
+            Runner("Усэйн", speed=10),
+            Runner("Андрей", speed=9),
+            Runner("Ник", speed=3),
+        ]
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in cls.all_results.values():
+            print(result)
+
+    def test_all_races(self):
+        for distance in range(10, 130, 10):
+            for group in range(2, len(self.runners) + 1):
+                for runners_combination in itertools.combinations(self.runners, group):
+                    tournament = Tournament(distance, *runners_combination)
+                    results = tournament.start()
+
+                    # Создаем словарь с номером места в качестве ключа и именем бегуна в качестве значения
+                    results_dict = {place: runner.name for place, runner in results.items()}
+
+                    sorted_results = sorted(runners_combination, key=lambda x: x.speed, reverse=True)
+                    result_names = [runner.name for runner in sorted_results]
+
+                    fastest_runner = result_names[0]
+                    slowest_runner = result_names[-1]
+
+                    print(f"Дистанция: {distance}м, Участники: {[runner.name for runner in runners_combination]}")
+                    print(f'Результаты: {results_dict}')  # Отображаем имена бегунов
+
+                    try:
+                        self.assertTrue(fastest_runner == results_dict[1],
+                                        f"Ошибка: {fastest_runner} не на первом месте на {distance}м")
+                    except AssertionError as e:
+                        print(f"Тест не прошел: {e}")
+
+                    try:
+                        self.assertTrue(slowest_runner == results_dict[len(sorted_results)],
+                                        f"Ошибка: {slowest_runner} не на последнем месте на {distance}м")
+                    except AssertionError as e:
+                        print(f"Тест не прошел: {e}")
+
+if __name__ == '__main__':
+    unittest.main()
