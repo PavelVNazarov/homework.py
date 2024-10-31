599db2d28c45ef11b6c7ae2be54ecad0e5bd91d4
PavelVNazarov <19pavel76@mail.ru>
2024-10-31 12:56:11 +0500

    Update test_12_2_best.py

--- module_12/test_12_2_best.py
+++ module_12/test_12_2_best.py
@@ -40,35 +40,33 @@ class TournamentTest(unittest.TestCase):
 
     def test_all_races(self):
         for distance in range(10, 130, 10):
-            #for group in range(2, len(self.runners) + 1):
-                #for runners_combination in itertools.combinations(self.runners, group):
-                for runners_combination in self.runners_group:
-                    tournament = Tournament(distance, *runners_combination)
-                    results = tournament.start()
-
-                    # Создаем словарь с номером места в качестве ключа и именем бегуна в качестве значения
-                    results_dict = {place: runner.name for place, runner in results.items()}
-
-                    sorted_results = sorted(runners_combination, key=lambda x: x.speed, reverse=True)
-                    result_names = [runner.name for runner in sorted_results]
-
-                    fastest_runner = result_names[0]
-                    slowest_runner = result_names[-1]
-
-                    print(f"Дистанция: {distance}м, Участники: {[runner.name for runner in runners_combination]}")
-                    print(f'Результаты: {results_dict}')  # Отображаем имена бегунов
-
-                    try:
-                        self.assertTrue(fastest_runner == results_dict[1],
-                                        f"Ошибка: {fastest_runner} не на первом месте на {distance}м")
-                    except AssertionError as e:
-                        print(f"Тест не прошел: {e}")
-
-                    try:
-                        self.assertTrue(slowest_runner == results_dict[len(sorted_results)],
-                                        f"Ошибка: {slowest_runner} не на последнем месте на {distance}м")
-                    except AssertionError as e:
-                        print(f"Тест не прошел: {e}")
+            for runners_combination in self.runners_group:
+                tournament = Tournament(distance, *runners_combination)
+                results = tournament.start()
+            
+                # Создаем словарь с номером места в качестве ключа и именем бегуна в качестве значения
+                results_dict = {place: runner.name for place, runner in results.items()}
+            
+                sorted_results = sorted(runners_combination, key=lambda x: x.speed, reverse=True)
+                result_names = [runner.name for runner in sorted_results]
+            
+                fastest_runner = result_names[0]
+                slowest_runner = result_names[-1]
+            
+                print(f"Дистанция: {distance}м, Участники: {[runner.name for runner in runners_combination]}")
+                print(f'Результаты: {results_dict}')  # Отображаем имена бегунов
+            
+                try:
+                    self.assertTrue(fastest_runner == results_dict[1],
+                                    f"Ошибка: {fastest_runner} не на первом месте на {distance}м")
+                except AssertionError as e:
+                    print(f"Тест не прошел: {e}")
+            
+                try:
+                    self.assertTrue(slowest_runner == results_dict[len(sorted_results)],
+                                    f"Ошибка: {slowest_runner} не на последнем месте на {distance}м")
+                except AssertionError as e:
+                    print(f"Тест не прошел: {e}")
 
 if __name__ == '__main__':
     unittest.main()
