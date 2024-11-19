73f8aa6b3ac6ce67423076250aa1e50e40203731
PavelVNazarov <19pavel76@mail.ru>
2024-11-19 17:55:17 +0500

    Update views.py

--- module_18/pythonProject1/UrbanDjango/task5/views.py
+++ module_18/pythonProject1/UrbanDjango/task5/views.py
@@ -30,5 +30,26 @@ def sign_up_by_django(request):
     return render(request, 'fifth_task/registration_page.html', info)
 
 def sign_up_by_html(request):
-    # Можно реализовать аналогично первому представлению
+    info = {}
+    if request.method == "POST":
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+            if password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+            elif age < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            elif username in users:
+                info['error'] = 'Пользователь уже существует'
+            else:
+                return render(request, 'fifth_task/registration_page.html', {"info": info, "success": f"Приветствуем, {username}!"})
+
+    else:
+        form = UserRegister()
+
+    info['form'] = form
     return sign_up_by_django(request)
