e93a0499206a9655ce23006003a812a7d998d864
PavelVNazarov <19pavel76@mail.ru>
2024-11-24 15:41:49 +0500

    Add files via upload

--- module_19/Urban19_5/myproject/blog/views.py
+++ module_19/Urban19_5/myproject/blog/views.py
@@ -3,9 +3,10 @@ from .models import Post
 from django.core.paginator import Paginator
 
 def post_list(request):
-    posts = Post.objects.all()
-    paginator = Paginator(posts, 5)  # Показывать 5 постов на странице
-
+    post_list = Post.objects.all()
+    post_count = int(request.GET.get('post_count', 5))  # По умолчанию 5
+    paginator = Paginator(post_list, post_count)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'blog/post_list.html', {'page_obj': page_obj})
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj, 'request': request})
+
